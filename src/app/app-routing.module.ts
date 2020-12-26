9a89b0b340a9c8c6856c6d77fe2118afc39e5d60
Gaurav <fillic2002@gmail.com>
2020-12-26 18:29:21 +0530

    Transaction "get"  working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { OptionComponent } from './option/option.component';
 
 import { SharesdetailComponent } from './sharesdetail/sharesdetail.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TransactionComponent } from './transaction/transaction.component';
 
 const routes: Routes = [
   {path:'', component:OptionComponent},
   {path:'shares',component:SharesdetailComponent},
   {path:'portfolio',component:PortfolioComponent},
+  {path:'transaction',component:TransactionComponent},
   
 ];
 
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SharesService } from '../shares.service';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-portfolio',
